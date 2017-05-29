1349db3ae0f90275041386722f52281d2bdaa635
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2017-05-29 22:13:06 +0200

    spelling fix

--- test/wall_test.py
+++ test/wall_test.py
@@ -8,9 +8,9 @@ class WallTest(unittest.TestCase):
     def setUp(self):
         self.initial_state = np.matrix('1, 1, 1; 2, 2, 2; 3, 3, 3')
         self.wall = Wall(self.initial_state)
-        self.wall_transponed = Wall(self.initial_state.transpose())
+        self.wall_transposed = Wall(self.initial_state.transpose())
 
-    def test_missing_bloks(self):
+    def test_missing_blocks(self):
         self.assertEqual(6, self.wall.missing_blocks())
 
     def test_peek_upper_row(self):
@@ -35,11 +35,11 @@ class WallTest(unittest.TestCase):
 
     def test_get_left_column(self):
         col = np.matrix('1;1;1')
-        self.assertTrue(np.array_equal(col, self.wall_transponed.get_left_column()))
+        self.assertTrue(np.array_equal(col, self.wall_transposed.get_left_column()))
 
     def test_get_right_column(self):
         col = np.matrix('3;3;3')
-        self.assertTrue(np.array_equal(col, self.wall_transponed.get_right_column()))
+        self.assertTrue(np.array_equal(col, self.wall_transposed.get_right_column()))
 
     def test_swap_upper_row(self):
         inserted_row = np.matrix('2,2,2')
@@ -55,14 +55,14 @@ class WallTest(unittest.TestCase):
 
     def test_swap_left_column(self):
         inserted_col = np.matrix('2;2;2')
-        returned_col = self.wall_transponed.swap_left_column(inserted_col)
-        self.assertTrue(np.array_equal(self.wall_transponed.get_left_column(), inserted_col))
+        returned_col = self.wall_transposed.swap_left_column(inserted_col)
+        self.assertTrue(np.array_equal(self.wall_transposed.get_left_column(), inserted_col))
         self.assertTrue(np.array_equal(returned_col, np.matrix('1;1;1')))
 
     def test_swap_right(self):
         inserted_col = np.matrix('2;2;2')
-        returned_col = self.wall_transponed.swap_right_column(inserted_col)
-        self.assertTrue(np.array_equal(self.wall_transponed.get_right_column(), inserted_col))
+        returned_col = self.wall_transposed.swap_right_column(inserted_col)
+        self.assertTrue(np.array_equal(self.wall_transposed.get_right_column(), inserted_col))
         self.assertTrue(np.array_equal(returned_col, np.matrix('3;3;3')))
 
 if __name__ == '__main__':
